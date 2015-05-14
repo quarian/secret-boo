0921ecbb9c5abc5587e20c22f2ab1dea41a0b30b
Miro Nurmela <miro.nurmela@gmail.com>
2015-05-14 15:14:48 +0300

    Housekeeping

--- shoe_main.ino
+++ shoe_main.ino
@@ -8,10 +8,19 @@
 #define LED_PIN            10
 
 #define HALL_SENS          A0
-#define FORCE_SENS_1       A2
-#define FORCE_SENS_2       A3
-#define FORCE_SENS_3       A4
-#define CURR_SENS          A5
+#define FORCE_SENSOR_1     A2
+#define FORCE_SENSOR_2     A3
+#define FORCE_SENSOR_3     A4
+#define CURRENT_SENSOR     A5
+
+#define STATE_INITIAL      20
+#define STATE_TIGHTENING   21
+#define STATE_CLOSED       22
+#define STATE_LOOSENING    23
+#define STATE_CONTROL      24
+
+#define CLOCKWISE          0
+#define COUNTERCLOCKWISE   1
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      1
@@ -21,7 +30,13 @@
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-int delayval = 10; // delay for half a second
+// color channel variables
+
+int RED = 0;
+int GREEN = 0;
+int BLUE = 0;
+
+int delayval = 10; // common delay value for the system
 
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
@@ -37,7 +52,7 @@ void setup() {
 }
 
 void loop() {
-  pixels.setPixelColor(0, pixels.Color(150,0,0)); // Moderately bright green color.
+  pixels.setPixelColor(0, pixels.Color(RED, GREEN, BLUE));
   pixels.show(); // This sends the updated pixel color to the hardware.
   delay(delayval); // Delay for a period of time (in milliseconds).
   
@@ -48,21 +63,23 @@ void loop() {
   int current = 0;
   for(int i = 0; i < 10; i++)
   {
-    current = analogRead(CURR_SENS);
+    current = analogRead(CURRENT_SENSOR);
     if(current != 0)
       break;
   }
   
   // Force sensors
-  int force1 = analogRead(FORCE_SENS_1);
-  int force2 = analogRead(FORCE_SENS_2);
-  int force3 = analogRead(FORCE_SENS_3);
+  int force1 = analogRead(FORCE_SENSOR_1);
+  int force2 = analogRead(FORCE_SENSOR_2);
+  int force3 = analogRead(FORCE_SENSOR_3);
+  
+  readForceSensors();
   
   // Switch
   int sw = digitalRead(SWITCH_PIN);
   
   // Chance motor direction
-  controlMotor(sw, force2/4);
+  controlMotor(sw, force2/6);
   
   // DEbug print
   Serial.print("Hall: ");
@@ -88,4 +105,33 @@ void controlMotor(boolean dir, int power)
     analogWrite(MOTOR_EN, power);
   else
     analogWrite(MOTOR_EN, 0);
+}
+
+void readForceSensors() {
+  int force1 = analogRead(FORCE_SENSOR_1);
+  int force2 = analogRead(FORCE_SENSOR_2);
+  int force3 = analogRead(FORCE_SENSOR_3);
+  
+  RED = force2 / 4;
+  BLUE = force3 / 4;
+}
+
+void readHallSensor() {
+    
+}
+
+void readHeelSwitch() {
+  
+}
+
+void resetShoe() {
+
+}
+
+void tightenLaces() {
+  
+}
+
+void loosenLaces() {
+  
 }
