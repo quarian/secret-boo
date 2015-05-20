e770d9dfb701132011b2e145c0382db37ea0f4da
Miro Nurmela <miro.nurmela@gmail.com>
2015-05-20 11:37:13 -0700

    I don't know anymore which one of these files is the proper one

---
+++ settings.h
@@ -0,0 +1,2 @@
+#define HALL_SENSOR_THRESHOLD     5      //  These two values are dictated by the placement of the hall sensor
+#define HALL_SENSOR_BASE_VALUE    565    //  and the magnets. Check these with the new buids!
